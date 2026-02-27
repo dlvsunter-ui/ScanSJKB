db91913583458def51b56c508d0a3ca032db622f
dlvsunter-ui <dlvsunter@gmail.com>
2026-02-27 14:59:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ======== KONFIGURASI FRONTEND ========
 // Ganti nilai di bawah ini dengan URL Web App Apps Script Anda
 // Contoh: const BACKEND_BASE = "https://script.google.com/macros/s/AKfycbx1234/exec";
-const BACKEND_BASE = ""; // <-- PASTIKAN DIISI
+const BACKEND_BASE = "https://script.google.com/macros/s/AKfycbyah2NEvBdsadpBpHI7-JGoeoe6cHtgkPKPF-aPxavaHHBuHjuTeEvDMTLoJeTMAz4x/exec"; // <-- PASTIKAN DIISI
 
 // PIN admin
 const ADMIN_PIN = "1235";
