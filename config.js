68010eb1375abd9ecadc649f3c6846926ea3ad8c
dlvsunter-ui <dlvsunter@gmail.com>
2026-03-03 14:50:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 // ======== KONFIGURASI FRONTEND ========
 // Ganti nilai di bawah ini dengan URL Web App Apps Script Anda
 // Contoh: const BACKEND_BASE = "https://script.google.com/macros/s/AKfycbx1234/exec";
-const BACKEND_BASE = "https://script.google.com/macros/s/AKfycbyah2NEvBdsadpBpHI7-JGoeoe6cHtgkPKPF-aPxavaHHBuHjuTeEvDMTLoJeTMAz4x/exec"; // <-- PASTIKAN DIISI
+const BACKEND_BASE = "https://script.google.com/macros/s/AKfycbyah2NEvBdsadpBpHI7-JGoeoe6cHtgkPKPF-aPxavaHHBuHjuTeEvDMTLoJeTMAz4x/exec"; // <-- ISI dgn URL Web App
 
 // PIN admin
 const ADMIN_PIN = "1235";
 
-// Jarak toleransi (meter) utk geofencing
+// Jarak toleransi (meter) utk geofencing proses Berangkat/Sampai
 const RADIUS_METERS = 600;
+
+// ====== ABSEN CONFIG ======
+const NVDC_NAME = "NVDC SUNTER"; // nama lokasi di master
+const ABSEN_ENTER_RADIUS = 500;   // tombol enable saat <= 500m
+const ABSEN_EXIT_RADIUS  = 650;   // reset lock saat >= 650m (hysteresis)
